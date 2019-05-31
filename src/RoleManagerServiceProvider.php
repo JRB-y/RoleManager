53342810f97265cb5cca2bd709616db7e7af8943
jrb <jrb.youssef@gmail.com>
2019-05-31 21:30:22 +0100

    add tag to the config file publisher

--- src/RoleManagerServiceProvider.php
+++ src/RoleManagerServiceProvider.php
@@ -9,9 +9,13 @@ class RoleManagerServiceProvider extends ServiceProvider{
     public function boot()
     {
         // publish configuration
+        // publish configuration
         $this->publishes([
             __DIR__.'/config/roles.php' => config_path('roles.php'),
-        ]);
+        ], 'roles');
+
+        // migrations
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
     }
 
     public function register()
