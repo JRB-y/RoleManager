00426a80c13c29f54fd663da82faa97fd16b776c
jrb <jrb.youssef@gmail.com>
2019-06-02 04:33:26 +0100

    refactor the service provider

--- src/RoleManagerServiceProvider.php
+++ src/RoleManagerServiceProvider.php
@@ -4,22 +4,62 @@ namespace Jrb\RoleManager;
 
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * Class RoleManagerServiceProvider
+ * @package Jrb\RoleManager
+ */
 class RoleManagerServiceProvider extends ServiceProvider{
 
+    /**
+     * Boot the Service.
+     *
+     * Publish the Configuration and the Migrations
+     */
     public function boot()
     {
-        // publish configuration
-        // publish configuration
+        $this->publishConfig();
+        $this->publishMigrations();
+    }
+
+    /**
+     * Register the Service.
+     *
+     * Load the Helpers.
+     */
+    public function register()
+    {
+        $this->loadHelpers();
+    }
+
+    /**
+     * Publish the configuration file (roles.php)
+     */
+    protected function publishConfig()
+    {
         $this->publishes([
             __DIR__.'/config/roles.php' => config_path('roles.php'),
         ], 'roles');
+    }
 
-        // migrations
+    /**
+     * Publish the migrations.
+     *
+     * Roles table and Role User pivot table.
+     */
+    protected function publishMigrations()
+    {
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
     }
 
-    public function register()
+    /**
+     *  Load all the helper functions files from the Helper dir.
+     */
+    protected function loadHelpers()
     {
-        //
+        foreach (glob(__DIR__.'/Helpers/*.php') as $filename)
+        {
+            require_once $filename;
+        }
     }
+
 }
